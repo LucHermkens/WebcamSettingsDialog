19872efbdbeb0f09993a6ca27fb062930ad00986
Luc Hermkens <info@luchermkens.nl>
2026-01-05 00:06:35 +0100

    build workflow fix

--- build.ps1
+++ build.ps1
@@ -1,4 +1,20 @@
-.\dotnet\dotnet.exe publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=false -p:EnableCompressionInSingleFile=true -p:PublishReadyToRun=true -p:IncludeNativeLibrariesForSelfExtract=true
+$publishArgs = @(
+    "publish"
+    "-c", "Release"
+    "-r", "win-x64"
+    "--self-contained", "true"
+    "-p:PublishSingleFile=true"
+    "-p:PublishTrimmed=false"
+    "-p:EnableCompressionInSingleFile=true"
+    "-p:PublishReadyToRun=true"
+    "-p:IncludeNativeLibrariesForSelfExtract=true"
+)
+
+if ((Get-Command dotnet -ErrorAction SilentlyContinue) -and ((dotnet --list-sdks | Select-String "^10\."))) {
+    dotnet @publishArgs
+} else {
+    .\dotnet\dotnet.exe @publishArgs
+}
 
 if ($LASTEXITCODE -eq 0) {
     Copy-Item -Path "bin\Release\net10.0-windows\win-x64\publish\WebcamSettings.exe" -Destination "WebcamSettings.exe"
